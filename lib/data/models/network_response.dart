18d13c22d9132b5394b2b9fde95f6464b5f598fe
parkjs <python2_7@naver.com>
2023-03-02 00:04:45 +0900

    [230301] 오타수정

--- lib/data/models/network_response.dart
+++ lib/data/models/network_response.dart
@@ -5,27 +5,22 @@ part 'network_response.freezed.dart';
 
 class NetworkResult<T> {
   final T data;
-
   NetworkResult({required this.data});
 }
 
-class NetworkSucess<T> {
+class NetworkSuccess<T> {
   final T data;
-
-  NetworkSucess({required this.data});
+  NetworkSuccess({required this.data});
 }
 
 class NetworkFail<T> {
   final T data;
-
   NetworkFail({required this.data});
 }
 
 @freezed
 class Result<T> with _$Result<T> {
   const factory Result.success({required T data}) = Success<T>;
-
   const factory Result.failure({required T data}) = Failure<T>;
-
   const factory Result.error({required String message}) = Error<T>;
 }
